97d0153474d0bf2e06e6bd2061fd28da3c7bf98e
Shankar <admin@shankytech.com>
2021-01-02 19:18:04 -0500

    updates and almost working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Listen for form submit
+/* Listen for form submit
 document.querySelector(".contact-form").addEventListener("submit", 
 submitForm);
 
@@ -17,3 +17,40 @@ function submitForm(e){
         //reset the form to blank
         document.querySelector(".contact-form").reset();
 }
+
+*/
+
+
+
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  let testForm = document.querySelector("#contact form");
+  
+  testForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(testForm);
+    fetch(testForm.getAttribute('action'), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      body: new URLSearchParams(formData).toString()
+    })
+    .then(res => {
+      if (res) {
+               //Show alert
+               document.querySelector('.alert').style.display = 'block';
+
+               //Hide alert after 3 seconds
+               setTimeout(function(){
+                   document.querySelector('.alert').style.display = 'none';   
+               }, 3000);
+           
+               //reset the form to blank
+               document.querySelector(".contact-form").reset();
+      }
+    });
+  });
+
